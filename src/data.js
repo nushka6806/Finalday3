408233b15373d2283223e903d5f60e76803a7e55
nushhkaa <bashyalanuska@gmail.com>
2024-07-19 16:30:24 +0545

    first commit

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const data = {
+    name: "Anuska Bashyal",
+    githubLink: "https://github.com/GITHUB_USERNAME" ,
+    email : "bashyalanuska@gmail.com",
+    hobbies : [
+        "none"
+    ],
+    jobTitle : "Works at student",
+    degree: "Bachelor's in Electroics Engineering",
+    campus: "Pulchowk Campus",
+    location: "Nepal",
+    spokenLanguages: "Nepali,English",
+}
+export default data;
